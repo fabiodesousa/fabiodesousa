50fbc13f5a9deac410a1217a7cb509f1e6b326a5
Fabio DeSousa <mail@fabiodesousa.com>
2018-07-16 11:56:10 -0400

    hub presentation link

---
+++ src/pages/tbih.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hub = () => (
+  <div
+    css={{
+      maxWidth: 960,
+      marginTop: '20vh',
+      marginLeft: '20vw',
+      marginRight: '20vw',
+    }}
+  >
+    <a href="https://docs.google.com/presentation/d/1KCfxgemIlEuhVWqHMwfwsTuwpbgVfzOk95Nf0VlWpBw/">Presentation</a>
+  </div>
+)
+
+export default Hub
