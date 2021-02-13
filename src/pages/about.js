bc357bc4ec8b12eb0638bde3d6a79a48d4ac0e94
Fabio DeSousa <fabiodesousa@Fabios-MacBook-Pro.local>
2021-02-13 18:30:19 -0500

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,8 +23,7 @@ const About = () => {
         <div css={css`
           max-width: 700px;
         `}>
-          <p>Fabio DeSousa is the Analytics and Engineering Manager at Embarc Collective. A Tampa native, Fabio graduated from Columbia University with a Bachelor of Arts in Sociology. Afterwards, Fabio became a Venture for America Fellow and worked for Pack Health, a digital health-coaching startup based in Birmingham, AL, as a Software Engineering Manager.</p>
-          <Link to={data.file.publicURL}>Here's his resume.</Link>
+          <p>Fabio DeSousa is Business Operations Manager at Remote and lives in Tampa, Florida. He'll get around to adding to this page at some point.</p>
         </div>
       </div>
     </Layout>
